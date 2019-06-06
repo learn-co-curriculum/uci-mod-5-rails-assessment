5c700d9a25ad9f0de710747113746bbb916fba2c
maxwellbenton <maxwellbenton@gmail.com>
2019-06-06 09:05:01 -0400

    adds solution controller actions

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,3 +1,15 @@
 class OwnersController < ApplicationController
-  
+  before_action :set_owner, only:[:show]
+  def index
+    @owners = Owner.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_owner
+    @owner = Owner.find_by(id: params[:id])
+  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,38 @@
 class PetsController < ApplicationController
-  
+  before_action :set_pet, only: [:show]
+
+  def index
+    if params[:search]
+      @pets = Pet.joins(:owner).where("owners.name LIKE ?", "%#{params[:search]}%")
+      @message = "There are no pets with that owner" if @pets.empty?
+    else
+      @pets = Pet.all
+    end
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :description, :owner_id)
+  end
+
+  def set_pet
+    @pet = Pet.find_by(id: params[:id])
+  end
 end
